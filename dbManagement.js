97b078462e4c86594a12a6567c22eaac99aef32f
Ruijia Brooks Niu <brooksnew92@gmail.com>
2021-11-21 20:46:45 -0800

    minor issue fixed

--- dbManagement.js
+++ dbManagement.js
@@ -1,7 +1,6 @@
 // get the CRUD Mongodb function
 // const MongoClient = require('mongodb').MongoClient;
 const mongoose = require("mongoose");
-const 
 const MongoUrl = "mongodb://127.0.0.1:27017";
 const dbName = "devil_dishes";
 
